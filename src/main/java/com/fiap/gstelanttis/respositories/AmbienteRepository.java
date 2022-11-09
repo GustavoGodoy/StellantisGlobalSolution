8882e58d50b4b688e7f81d50abf35107d1acaec5
Gustavo <gustavocoliver@gmail.com>
2022-11-09 10:41:58 -0300

    feat: repositories

---
+++ src/main/java/com/fiap/gstelanttis/respositories/AmbienteRepository.java
@@ -0,0 +1,7 @@
+package com.fiap.gstelanttis.respositories;
+
+import com.fiap.gstelanttis.models.Ambiente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AmbienteRepository extends JpaRepository<Ambiente, Long> {
+}
