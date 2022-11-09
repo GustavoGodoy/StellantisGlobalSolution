f72323471f632af52a03b221673098fb4efb754b
Gustavo <gustavocoliver@gmail.com>
2022-11-09 10:45:17 -0300

    feat: application config

---
+++ src/test/java/com/fiap/gstelanttis/GStelanttisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gstelanttis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GStelanttisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
